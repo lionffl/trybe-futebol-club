8540d5308ea39c4c605f5205bbdc87e96ec1bb4e
Felipe <lionffl@gmail.com>
2022-10-21 22:15:30 -0300

    tests: added /login tests to invalid email or password

--- app/backend/src/tests/UserModel.test.ts
+++ app/backend/src/tests/UserModel.test.ts
@@ -3,7 +3,7 @@ import * as chai from 'chai';
 import chaiHttp from 'chai-http';
 import { app } from '../app';
 import UserModel from '../database/models/UserModel';
-import { validLogin, validUser, invalidUser } from './mocks';
+import { validLogin, validUser, invalidLoginPassword, invalidLoginEmail } from './mocks';
 import { Response } from 'superagent';
 
 chai.use(chaiHttp);
@@ -37,7 +37,7 @@ context('Route /login tests', () => {
       it('should return 400 if email is missing', async () => {
         httpResponse = await chai
           .request(app)
-          .post('/login').send({ password: '1234565123123' })
+          .post('/login').send({ password: '' })
 
         expect(httpResponse.status).to.equal(400)
         expect(httpResponse.body).to.be.deep.equal({ message: 'All fields must be filled' })
@@ -46,10 +46,28 @@ context('Route /login tests', () => {
       it('should return 400 if password is missing', async () => {
         httpResponse = await chai
           .request(app)
-          .post('/login').send({ email: 'email@gmail.com' })
+          .post('/login').send({ email: '' })
 
         expect(httpResponse.status).to.equal(400)
         expect(httpResponse.body).to.be.deep.equal({ message: 'All fields must be filled' })
+      });
+
+      it('should return 401 if email is invalid', async () => {
+        httpResponse = await chai
+          .request(app)
+          .post('/login').send(invalidLoginEmail)
+
+        expect(httpResponse.status).to.equal(401)
+        expect(httpResponse.body).to.be.deep.equal({ message: 'Incorrect email or password' })
+      });
+      
+      it('should return 401 if password is invalid', async () => {
+        httpResponse = await chai
+          .request(app)
+          .post('/login').send(invalidLoginPassword)
+
+        expect(httpResponse.status).to.equal(401)
+        expect(httpResponse.body).to.be.deep.equal({ message: 'Incorrect email or password' })
       });
     });
   });
--- app/backend/src/tests/mocks.ts
+++ app/backend/src/tests/mocks.ts
@@ -1,18 +1,21 @@
-export const invalidUser = {
-  username: "lion",
-  role: "impostor",
-  email: "lion@gmail.com",
-  password: "lionffl",
-};
-
 export const validUser = {
-  username: "Admin",
-  role: "admin",
-  email: "admin@admin.com",
-  password: "$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW",
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
 };
 
 export const validLogin = {
-  email: "admin@admin.com",
-  password: "secret_admin",
+  email: 'admin@admin.com',
+  password: 'secret_admin',
 };
+
+export const invalidLoginPassword = {
+  email: 'admin@admin.com',
+  password: '1231321r345235145'
+}
+
+export const invalidLoginEmail = {
+  email: 'lionffl@gmail.com',
+  password: '1231321r345235145'
+}
